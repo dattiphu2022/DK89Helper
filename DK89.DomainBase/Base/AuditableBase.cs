e5f3b25248940e535c352249c6ac7e969c786cbd
Game Master <friendlyhospital@gmail.com>
2025-01-14 10:08:03 +0700

    Add project files.

---
+++ DK89.DomainBase/Base/AuditableBase.cs
@@ -0,0 +1,28 @@
+﻿using DK89.DomainBase.Interface;
+using System;
+
+namespace DK89.DomainBase.Base
+{
+    /// <summary>
+    /// Default ID has GUID type.
+    /// </summary>
+    public abstract class AuditableBase : IAuditable
+    {
+        public virtual Guid Id { get; set; } = default!;
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? EditedBy { get; set; }
+        public DateTime? EditedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+    public abstract class AuditableBase<TIdType> : IAuditable
+    {
+        public virtual TIdType Id { get; set; } = default!;
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? EditedBy { get; set; }
+        public DateTime? EditedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
+
---
+++ DK89.DomainBase/Interface/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DK89.DomainBase.Interface
+{
+    public interface IAuditable
+    {
+        string? CreatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        string? EditedBy { get; set; }
+        DateTime? EditedDate { get; set; }
+        bool IsDeleted { get; set; }
+    }
+
+}
